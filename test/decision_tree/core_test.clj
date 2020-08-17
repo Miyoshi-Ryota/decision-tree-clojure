1cc4837dcaf07181d3b2d3022433d824669792d7
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2020-08-17 18:45:10 +0900

    Add tests

--- test/decision_tree/core_test.clj
+++ test/decision_tree/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [decision-tree.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-update-key
+  (testing "expectation use case of the function"
+    (let [input-data {:Sepal.Length 5.1, :Sepal.Width 3.5, :Petal.Length 1.4, :Petal.Width 0.2, :Species "setosa"}
+          expected-output-data {:Sepal.Length 5.1, :Sepal.Width 3.5, :Petal.Length 1.4, :Petal.Width 0.2, :Classes "setosa"}]
+      (is (= expected-output-data
+             (#'decision-tree.core/update-key :Species :Classes input-data))))))
+
+(deftest test-gini-impurity
+  (testing "expectation use case of the function"
+    (let [data-expected-zero '("a" "a" "a")
+          data-expected-two-thirds '("a" "b" "c")]
+      (is (= 0.0 (#'decision-tree.core/gini-impurity data-expected-zero)))
+      (is (= (double (/ 2 3)) (#'decision-tree.core/gini-impurity data-expected-two-thirds))))))
